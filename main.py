771ba468aaf4cfbd6a32344345b7234648dc8d69
Saksham Sharma <saksham.sharma@uwaterloo.ca>
2021-06-05 01:45:17 +0530

    finished the game

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import os
 import random
 import sys
 
+from pygame import image
+
 
 # -----------------  Initialize pygame  ----------------- #
 
@@ -25,12 +27,21 @@ RUNNING = [pygame.image.load(os.path.join("assets/dino", "DinoRun1.png")),
 
 JUMPING = pygame.image.load(os.path.join("assets/dino", "DinoJump.png"))
 
+SMALL_CACTUS = [pygame.image.load(os.path.join("assets/cactus", "SmallCactus1.png")),
+                pygame.image.load(os.path.join(
+                    "assets/cactus", "SmallCactus2.png")),
+                pygame.image.load(os.path.join("assets/cactus", "SmallCactus3.png"))]
+LARGE_CACTUS = [pygame.image.load(os.path.join("assets/cactus", "LargeCactus1.png")),
+                pygame.image.load(os.path.join(
+                    "assets/cactus", "LargeCactus2.png")),
+                pygame.image.load(os.path.join("assets/cactus", "LargeCactus3.png"))]
+
 GROUND = pygame.image.load(os.path.join("assets/other", "Track.png"))
 
 FONT = pygame.font.Font('freesansbold.ttf', 20)
 
 
-# -----------------  Dino Class  ----------------- #
+# -----------------  Object classes  ----------------- #
 
 
 class Dino:
@@ -75,14 +86,70 @@ class Dino:
         SCREEN.blit(self.imgage, (self.rect.x, self.rect.y))
 
 
-# -----------------  main() function  ----------------- #
+class Obstacle:
+    def __init__(self, image, number_of_cacti):
+        self.image = image
+        self.type = number_of_cacti
+        self.rect = self.image[self.type].get_rect()
+        self.rect.x = SCREEN_WIDTH
+
+    def update(self):
+        self.rect.x -= game_speed
+        if self.rect.x < -self.rect.width:
+            obstacles.pop()
+
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image[self.type], self.rect)
+
+
+class SmallCactus(Obstacle):
+    def __init__(self, image, number_of_cacti):
+        super().__init__(image, number_of_cacti)
+        self.rect.y = 325
+
+
+class LargeCactus(Obstacle):
+    def __init__(self, image, number_of_cacti):
+        super().__init__(image, number_of_cacti)
+        self.rect.y = 300
+
+
+# -----------------  Functions  ----------------- #
+
+
+def remove(index):
+    dinos.pop(index)
 
 
 def main():
+    global game_speed, x_pos_bg, y_pos_bg, obstacles, dinos,  points
     clock = pygame.time.Clock()
+    points = 0
 
+    obstacles = []
     # Ability to display multiple dinos for AI implementation
     dinos = [Dino()]
+
+    x_pos_bg = 0
+    y_pos_bg = 380
+    game_speed = 20
+
+    def score():
+        global points, game_speed
+        points += 1
+        if points % 100 == 0:
+            game_speed += 1
+        text = FONT.render(f'Points: {str(points)}', True, (0, 0, 0))
+        SCREEN.blit(text, (950, 50))
+
+    def background():
+        global x_pos_bg, y_pos_bg
+        image_width = GROUND.get_width()
+        SCREEN.blit(GROUND, (x_pos_bg, y_pos_bg))
+        SCREEN.blit(GROUND, (image_width + x_pos_bg, y_pos_bg))
+        if x_pos_bg <= -image_width:
+            x_pos_bg = 0
+        x_pos_bg -= game_speed
 
     run = True
     while(run):
@@ -96,6 +163,25 @@ def main():
             dino.update()
             dino.draw(SCREEN)
 
+        if len(dinos) == 0:
+            break
+
+        if len(obstacles) == 0:
+            rand_int = random.randint(0, 1)
+            if rand_int == 0:
+                obstacles.append(SmallCactus(
+                    SMALL_CACTUS, random.randint(0, 2)))
+            elif rand_int == 1:
+                obstacles.append(LargeCactus(
+                    LARGE_CACTUS, random.randint(0, 2)))
+
+        for obstacle in obstacles:
+            obstacle.draw(SCREEN)
+            obstacle.update()
+            for i, dinosaur in enumerate(dinos):
+                if dinosaur.rect.colliderect(obstacle.rect):
+                    remove(i)
+
         user_input = pygame.key.get_pressed()
 
         for (i, dino) in enumerate(dinos):
@@ -103,6 +189,8 @@ def main():
                 dino.dino_jump = True
                 dino.dino_run = False
 
+        score()
+        background()
         clock.tick(30)
         pygame.display.update()
 
